cfafebed3cc796deced5039f655170d1c9a61f55
Diego Bejar <diego.bejar@tecsup.edu.pe>
2025-05-30 13:29:53 -0500

    primer commit

---
+++ index.php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi página PHP</title>
+</head>
+<body>
+    <h1>Hola, mundo en PHP</h1>
+    <?php
+        echo "Esto fue generado con PHP";
+    ?>
+</body>
+</html>
